8e8619919729d0e7f68e815cb926e373fdc87122
vinayaachar <vinayaachar@gmail.com>
2021-01-14 19:36:29 -0800

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,9 +1,26 @@
-CREATE DATABASE chat;
+CREATE DATABASE chatNew;
 
-USE chat;
+USE chatNew;
+
+CREATE TABLE rooms (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  PRIMARY KEY (id)
+);
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  message VARCHAR(500),
+  user_id INT NOT NULL,
+  room_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (room_id) REFERENCES rooms(id)
 );
 
 /* Create other tables and define schemas for them here! */
