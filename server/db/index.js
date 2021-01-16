e95a30823ebcb98efe23087484c5161c80a7d3d9
vinayaachar <vinayaachar@gmail.com>
2021-01-15 18:11:48 -0800

    Complete customizing tests

--- server/db/index.js
+++ server/db/index.js
@@ -4,4 +4,16 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
+var dbConnection = mysql.createConnection({
+  user: 'root',
+  database: 'chatNew'
+});
+
+dbConnection.connect(function(err) {
+  if (err) {throw err;}
+});
+
+module.exports = dbConnection;
+
+
 
--- server/models/index.js
+++ server/models/index.js
@@ -2,14 +2,32 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function () {
+      db.query('SELECT * FROM messages', (err, results, fields) => {
+        if(err) { throw err; }
+      });
+    }, // a function which produces all the messages
+    post: function (currentMessage) {
+      var messagePost = `INSERT INTO messages (message) VALUES ($(currentMessage))`;
+      db.query(messagePost, function (err, result) {
+        if (err) { throw err; }
+      });
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function () {
+      db.query('SELECT * FROM users', (err, results, fields) => {
+        if (err) { throw err; }
+      });
+    },
+    post: function (userName) {
+      var userPost = `INSERT INTO users (name) VALUES ($(userName))`;
+      db.query(userPost, function (err, result) {
+        if (err) { throw err; }
+      });
+    }
   }
 };
 
