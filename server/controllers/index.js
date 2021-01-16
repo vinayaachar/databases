14a9eb779e2c5abf2d606a5fc705942f1c8e8682
vinayaachar <vinayaachar@gmail.com>
2021-01-16 14:34:21 -0800

    Complete refactoring with Sequelize

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,25 +1,41 @@
+const { Message, User } = require('../db');
 var models = require('../models');
 
 module.exports = {
   messages: {
     get: function (req, res) {
-      models.messages.get((err, data) => {
-        if (err) {
-          thow(err);
-        } else {
-          res.send(data);
-        }
-      });
+      Message.findAll({ include: [User]})
+        .complete(function(err, results) {
+          res.json(results);
+        });
+      // models.messages.get((err, data) => {
+      //   if (err) {
+      //     thow(err);
+      //   } else {
+      //     res.send(data);
+      //   }
+      // });
       //next();
     }, // a function which handles a get request for all messages
     post: function (req, res) {
-      models.messages.post(req.body.message, (err, data) => {
-        if (err) {
-          throw (err);
-        } else {
-          res.sendStatus(200);
-        }
-      });
+      User.findorCreate({username: req.body.username})
+        .complete(function(err, user) {
+          var params = {
+            message: req.body.message,
+            roomname: req.body.roomname
+          };
+          Message.create(params)
+            .complete((err, results) => {
+              res.sendStatus(201);
+            });
+        });
+      // models.messages.post(req.body.message, (err, data) => {
+      //   if (err) {
+      //     throw (err);
+      //   } else {
+      //     res.sendStatus(200);
+      //   }
+      // });
       //next();
     } // a function which handles posting a message to the database
   },
@@ -27,25 +43,34 @@ module.exports = {
   users: {
     // Ditto as above
     get: function (req, res) {
-      models.users.get((err, data) => {
-        if (err) {
-          thow(err);
-        } else {
-          res.send(data);
-        }
-      });
+      User.findAll()
+        .complete(function(err, results) {
+          res.json(results);
+        });
+
+      // models.users.get((err, data) => {
+      //   if (err) {
+      //     thow(err);
+      //   } else {
+      //     res.send(data);
+      //   }
+      // });
       //next();
     }, // a function which handles a get request for all messages
     post: function (req, res) {
-      console.log('Controller', req.body);
-      models.users.post(req.body.username, (err) => {
-        if (err) {
-          throw (err);
-        } else {
-          //res.send('Successfully posted', data);
-          res.sendStatus(200);
-        }
-      });
+      User.create({username: req.body.username})
+        .complete((err, user) => {
+          res.sendStatus(201);
+        });
+      // console.log('Controller', req.body);
+      // models.users.post(req.body.username, (err) => {
+      //   if (err) {
+      //     throw (err);
+      //   } else {
+      //     //res.send('Successfully posted', data);
+      //     res.sendStatus(200);
+      //   }
+      // });
       //next();
     }
   }
--- server/db/index.js
+++ server/db/index.js
@@ -1,20 +1,46 @@
 var mysql = require('mysql');
 
-// Create a database connection and export it from this file.
-// You will need to connect with the user "root", no password,
-// and to the database "chat".
+var Sequelize = require('Sequelize');
+var rom = new Sequelize('chatnew', 'root');
 
-var dbConnection = mysql.createConnection({
-  user: 'root',
-  password: '',
-  database: 'chatnew'
+var User = orm.define('User', {
+  username: Sequelize.STRING
 });
 
-dbConnection.connect(function(err) {
-  if (err) {throw err;}
+var Message = orm.define('Message', {
+  username: Sequelize.STRING,
+  message: Sequelize.STRING,
+  roomname: Sequelize.STRING
 });
 
-module.exports = dbConnection;
+User.hasMany(Message);
+Message.belongsTo(User);
+
+User.sync();
+Message.sync();
+
+exports.User = User;
+exports.Message = Message;
+
+
+
+
+
+// // Create a database connection and export it from this file.
+// // You will need to connect with the user "root", no password,
+// // and to the database "chat".
+
+// var dbConnection = mysql.createConnection({
+//   user: 'root',
+//   password: '',
+//   database: 'chatnew'
+// });
+
+// dbConnection.connect(function(err) {
+//   if (err) {throw err;}
+// });
+
+// module.exports = dbConnection;
 
 
 
